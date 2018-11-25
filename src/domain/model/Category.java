758079fb7373da6f934847d612d05e5fcdcb7003
tvermeersch <tvermeersch@protonmail.com>
2018-11-25 21:14:19 +0100

    added category class

--- src/domain/model/Category.java
+++ src/domain/model/Category.java
@@ -1,4 +1,47 @@
 package domain.model;
 
 class Category {
+    private String title;
+    private String description;
+    private Category mainCategory;
+
+    Category(String title, String description, Category mainCategory) {
+        setTitle(title);
+        setDescription(description);
+        setMainCategory(mainCategory);
+    }
+
+    Category(String title, String description) {
+        this(title, description, null);
+    }
+
+    private void setTitle(String title) {
+        if (title.isEmpty()) {
+            throw new ModelException("cannot make category with empty title");
+        }
+        this.title = title;
+    }
+
+    private void setDescription(String description) {
+        if (description.isEmpty()) {
+            throw new ModelException("cannot make category with empty description");
+        }
+        this.description = description;
+    }
+
+    private void setMainCategory(Category mainCategory) {
+        this.mainCategory = mainCategory;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Category getMainCategory() {
+        return mainCategory;
+    }
 }
