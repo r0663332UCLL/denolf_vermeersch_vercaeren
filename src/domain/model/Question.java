a11a2c370ef1294738f1bb45cf7c2d3e0b94ea64
tvermeersch <tvermeersch@protonmail.com>
2018-11-25 19:35:23 +0100

    added a category to Question class

--- src/domain/model/Question.java
+++ src/domain/model/Question.java
@@ -1,16 +1,20 @@
 package domain.model;
 
+import com.sun.org.apache.xpath.internal.operations.Mod;
+
 import java.util.ArrayList;
 
 abstract class Question {
     private String question;
     private ArrayList<String> statements;
     private String feedback;
+    private Category category;
 
-    public Question(String question, ArrayList<String> statements, String feedback) {
+    public Question(String question, ArrayList<String> statements, String feedback, Category category) {
         setQuestion(question);
         setStatements(statements);
         setFeedback(feedback);
+        setCategory(category);
     }
 
     public String getQuestion() {
@@ -41,6 +45,17 @@ abstract class Question {
             throw new ModelException("Cannot make question with empty question string");
         }
         this.feedback = feedback.trim();
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    private void setCategory(Category category) {
+        if (category == null) {
+            throw new ModelException("Cannot make a question with empty category");
+        }
+        this.category = category;
     }
 
     public String getCorrectStatement() {
