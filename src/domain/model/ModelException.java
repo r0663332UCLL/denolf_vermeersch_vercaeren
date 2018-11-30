fe42e699cdbbf2c5444f34f91dcbc036d18f3e99
tvermeersch <tvermeersch@protonmail.com>
2018-12-01 00:17:34 +0100

    removed faulty modelexception code

--- src/domain/model/ModelException.java
+++ src/domain/model/ModelException.java
@@ -4,16 +4,16 @@ class ModelException extends RuntimeException {
     ModelException() {
         super();
     }
+
     ModelException(String message, Throwable exception) {
         super(message, exception);
     }
+
     ModelException(String message) {
         super(message);
     }
+
     ModelException(Throwable exception) {
         super(exception);
-    }
-
-    public ModelException(void printStackTrace) {
     }
 }
