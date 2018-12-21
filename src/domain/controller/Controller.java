e6e16e3d056192b95afb8966beadd0c3de0e09ba
ThomasVermeersch <thomasvermeerschv@gmail.com>
2018-12-21 20:40:54 +0100

    Add new Category functionality to ui

--- src/domain/controller/Controller.java
+++ src/domain/controller/Controller.java
@@ -2,15 +2,25 @@ package domain.controller;
 
 import domain.controller.Handlers.HandlerFactory;
 import domain.model.ApplicationService;
+import domain.model.Category;
+
 import java.util.ArrayList;
 
 public class Controller {
     private ApplicationService service = new ApplicationService();
     private HandlerFactory factory = new HandlerFactory();
-    public void DoAction (String action, ArrayList<Object> parameters) {
+    public void doAction (String action, ArrayList<Object> parameters) {
         if (action == null) {
             throw new ControllerException("action cannot be empty");
         }
         factory.getHandler(action, service).HandleRequest(parameters);
     }
+
+    public ArrayList<Object> doActionWithReturnValue(String action, ArrayList<Object> parameters) {
+        if (action == null) {
+            throw new ControllerException("action cannot be empty");
+        }
+        return (ArrayList<Object>) factory.getHandler(action, service).HandleRequest(parameters);
+    }
+
 }
--- src/view/panels/CategoryOverviewPane.java
+++ src/view/panels/CategoryOverviewPane.java
@@ -1,5 +1,6 @@
 package view.panels;
 
+import domain.controller.Controller;
 import domain.model.ApplicationService;
 import domain.model.Category;
 import javafx.collections.FXCollections;
@@ -7,6 +8,8 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
@@ -14,19 +17,21 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 
 public class CategoryOverviewPane extends GridPane {
 	private TableView table;
 	private Button btnNew;
-	//TODO  USE CONTROLLER INSTEAD OF SERVICE
-	ApplicationService service = new ApplicationService();
-	ObservableList<Category> data = FXCollections.observableArrayList(service.getCategories());
+	Controller controller = new Controller();
+	ObservableList<Object> data = FXCollections.observableArrayList(controller.doActionWithReturnValue("GetCategory", new ArrayList<>()));
 	public CategoryOverviewPane() {
 		this.setPadding(new Insets(5, 5, 5, 5));
         this.setVgap(5);
         this.setHgap(5);
-        
 		this.add(new Label("Categories:"), 0, 0, 1, 1);
 		
 		table = new TableView<>();
@@ -41,6 +46,18 @@ public class CategoryOverviewPane extends GridPane {
 		table.setItems(data);
 		btnNew = new Button("New");
 		this.add(btnNew, 0, 11, 1, 1);
+		btnNew.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				CategoryDetailPane categoryDetailPane = new CategoryDetailPane();
+				final Stage categoryDetail = new Stage();
+				Group root = new Group();
+				Scene scene = new Scene(root);
+				root.getChildren().add(categoryDetailPane);
+				categoryDetail.setScene(scene);
+				categoryDetail.show();
+			}
+		});
 	}
 
     public void setNewAction(EventHandler<ActionEvent> newAction) {
